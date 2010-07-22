72640e2071c76ff6d2318ae8a69bb4333d92837f
Brandon Turner <bturner@bltweb.net>
2010-07-22 18:04:47 -0500

    Fixed bug on root node fetched via fetchBranch()
    Fixed an issue where the root node of a tree fetched via the
    fetchBranch() or fetchBranchAsArray() methods didn't respect custom joins
    defined in Config's base query builder.

--- lib/DoctrineExtensions/NestedSet/Manager.php
+++ lib/DoctrineExtensions/NestedSet/Manager.php
@@ -175,6 +175,12 @@ class Manager
             return array();
         }
 
+        // Detach the fetched node as Doctrine uses it instead of the node
+        // fetched as part of the tree.  The node fetched as part of the tree
+        // may have additional joined classes via base query builder.
+        $this->getEntityManager()->detach($node);
+
+
         $qb = $config->getBaseQueryBuilder();
         $alias = $config->getQueryBuilderAlias();
 
